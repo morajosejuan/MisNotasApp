bc49b6d8e1265b6b47315a5023c428784a949e00
Jose Juan <jjmoraherrera@gmail.com>
2022-11-29 22:41:16 -0500

    navegación

--- app/src/main/java/com/zzc/misnotasapp/MainActivity.java
+++ app/src/main/java/com/zzc/misnotasapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.zzc.misnotasapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void ingreso(View view){
+        Intent ingresar = new Intent(MainActivity.this, IngresarActivity.class);
+            startActivity(ingresar);
+        }
+
+    public void nota(View view){
+        Intent notasycurso = new Intent(MainActivity.this, NotayCursoActivity.class);
+        startActivity(notasycurso);
+    }
+
+    public void horario(View view){
+        Intent horarioytareas = new Intent(MainActivity.this, HorarioyTareasActivity.class);
+        startActivity(horarioytareas);
+    }
+
+    public void estudiante(View view){
+        Intent estudiantes = new Intent(MainActivity.this, EstudianteActivity.class);
+        startActivity(estudiantes);
+    }
+
+    public void promedio(View view){
+        Intent calcularpromedio = new Intent(MainActivity.this, CalcularPromedioActivity.class);
+        startActivity(calcularpromedio);
+    }
+
 }
