e8a9c218f106fc1871389ff8dbbcda701d1505cc
Jose Juan <jjmoraherrera@gmail.com>
2022-11-29 22:15:39 -0500

    first commit

---
+++ app/src/main/java/com/zzc/misnotasapp/HorarioyTareasActivity.java
@@ -0,0 +1,14 @@
+package com.zzc.misnotasapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HorarioyTareasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_horarioy_tareas);
+    }
+}
---
+++ app/src/main/java/com/zzc/misnotasapp/NotayCursoActivity.java
@@ -0,0 +1,14 @@
+package com.zzc.misnotasapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NotayCursoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notay_curso);
+    }
+}
